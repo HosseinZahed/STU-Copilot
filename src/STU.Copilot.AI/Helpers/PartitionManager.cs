7908fa5735ce1a1649c1ede585ada5ac0776eb78
Hossein Zahed <hzahed@outlook.com>
2025-05-10 22:14:03 +0200

    CosmosDB partition manager helper added.

---
+++ src/STU.Copilot.AI/Helpers/PartitionManager.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Azure.Cosmos;
+
+namespace STU.Copilot.AI.Helpers;
+
+public static class PartitionManager
+{
+    public static PartitionKey GetChatDataFullPK(string tenantId, string userId, string sessionId)
+    {
+        var partitionKey = new PartitionKeyBuilder()
+            .Add(tenantId)
+            .Add(userId)
+            .Add(sessionId)
+            .Build();
+        return partitionKey;
+    }
+
+    public static PartitionKey GetChatDataPartialPK(string tenantId, string userId)
+    {
+        var partitionKey = new PartitionKeyBuilder()
+            .Add(tenantId)
+            .Add(userId)
+            .Build();
+        return partitionKey;
+    }
+
+    public static PartitionKey GetAccountsDataFullPK(string tenantId, string accountId)
+    {
+        var partitionKey = new PartitionKeyBuilder()
+            .Add(tenantId)
+            .Add(accountId)
+            .Build();
+        return partitionKey;
+    }
+
+    public static PartitionKey GetAccountsPartialPK(string tenantId)
+    {
+        var partitionKey = new PartitionKeyBuilder()
+            .Add(tenantId)
+            .Build();
+        return partitionKey;
+    }
+
+    public static PartitionKey GetUserDataFullPK(string tenantId)
+    {
+        var partitionKey = new PartitionKeyBuilder()
+            .Add(tenantId)
+            .Build();
+        return partitionKey;
+    }
+}
