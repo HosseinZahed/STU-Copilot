e25a74261a32015f8b97e69781069a2f8a931065
Hossein Zahed <hzahed@outlook.com>
2025-05-10 22:50:49 +0200

    Dependency injection added.

--- src/STU.Copilot.AI/Models/Configuration/AzureOpenAISettings.cs
+++ src/STU.Copilot.AI/Models/Configuration/AzureOpenAISettings.cs
@@ -1,8 +1,6 @@
 ﻿namespace STU.Copilot.AI.Models.Configuration;
 public record AzureOpenAISettings
 {
-    public const string SectionName = "AzureOpenAI";
-
     public required string ChatDeploymentName { get; set; }
 
     public required string EmbeddingDeploymentName { get; set; }
---
+++ src/STU.Copilot.AI/Services/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using STU.Copilot.AI.Models.Configuration;
+
+namespace STU.Copilot.AI.Services;
+
+public static partial class DependencyInjection
+{
+    public static void AddSemanticKernelService(this IHostApplicationBuilder builder)
+    {
+        builder.Services.AddOptions<SemanticKernelServiceSettings>()
+            .Bind(builder.Configuration.GetSection("SemanticKernelServiceSettings"));
+
+        builder.Services.AddSingleton<ISemanticKernelService, SemanticKernelService>();
+    }
+
+    public static void AddCosmosDBService(this IHostApplicationBuilder builder)
+    {
+        builder.Services.AddOptions<CosmosDBSettings>()
+            .Bind(builder.Configuration.GetSection("CosmosDBSettings"));
+        builder.Services.AddSingleton<ICosmosDBService, CosmosDBService>();
+    }
+
+    public static void AddChatService(this IHostApplicationBuilder builder)
+    {
+        builder.Services.AddSingleton<IChatService, ChatService>();
+    }
+}
